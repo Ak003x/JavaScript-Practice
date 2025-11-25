9aec62272e0bf0bad11c96015de4ff7e560d24ff
Dev_Ak <aakash.velu.003@gmail.com>
2025-11-25 16:48:55 +0530

    sum2

--- Code/Sums.js
+++ Code/Sums.js
@@ -1,11 +1,23 @@
-//*1.object sum
-const obj = {
-    a: 1,
-    b: 3,
-    sum() {
-        return this.a + this.b;
-    }
+//* 1.problem
+//* object sum
+
+// const obj = {
+//     a: 1,
+//     b: 3,
+//     sum() {
+//         return this.a + this.b;
+//     }
+// }
+
+// console.log(obj.sum());
+
+//* 2.problem
+//*solve the problem
+
+for (let i = 0; i < 3; i++) {
+    setTimeout(() => {
+        console.log(i);
+    }, 100)
 }
 
-const res = obj.sum();
-console.log(res);
+// to solve we have to add let
