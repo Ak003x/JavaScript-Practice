db2638d2196dd0badd64a726f4e8b8386832149e
Dev_Ak <aakash.velu.003@gmail.com>
2025-11-25 17:00:48 +0530

    Sum3

--- Code/Sums.js
+++ Code/Sums.js
@@ -14,10 +14,26 @@
 //* 2.problem
 //*solve the problem
 
-for (let i = 0; i < 3; i++) {
-    setTimeout(() => {
-        console.log(i);
-    }, 100)
-}
+// for (let i = 0; i < 3; i++) {
+//     setTimeout(() => {
+//         console.log(i);
+//     }, 100)
+// }
 
 // to solve we have to add let
+
+
+//*3.problem About Objects
+
+//! Creation of Object
+// const obj = {
+//     name: "ak",
+//     age: 45,
+// }
+//! To access 
+// console.log(obj.name);
+//! Modify
+// obj.name = "Akash";
+//! Delete
+// delete obj.age;
+// console.log(obj);
