da46aaad28c5cc3dd447f74f7ddc58aa957c1a7c
Dev_Ak <aakash.velu.003@gmail.com>
2025-11-29 22:54:50 +0530

    sum 8

--- Code/Sums.js
+++ Code/Sums.js
@@ -91,3 +91,19 @@
 //     return obj;
 // }
 // console.log(multipleOf2(nums));
+
+//*8 What is the output
+
+
+const shape = {
+    radius: 10,
+    diameter() {
+        return this.radius * 2;
+    },
+
+
+    perimeter: () => 2 * Math.PI * this.radius,
+}
+
+console.log(shape.diameter());
+console.log(shape.perimeter());
