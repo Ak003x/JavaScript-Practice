68b43b64922f5c01255e775399e4299e197ddffe
Dev_Ak <aakash.velu.003@gmail.com>
2026-01-31 01:53:25 +0530

    ...

--- Code/practice.js
+++ Code/practice.js
@@ -730,7 +730,11 @@
 // email = email.toUpperCase();
 // console.log(email);
 
-let str = "javaScript is a programming language"
-let str1 = str.replace("is","was");//to replace 
+let str = "javaScript is and programming language"
+let str1 = str.replace("is", "was");//to replace 
 str.includes("javaScript")
-console.log(str1);
+let str2 = str.slice(0, str.indexOf("and"));
+// console.log(str1);
+console.log("--".repeat(10));
+console.log(str2);
+console.log("--".repeat(10));
